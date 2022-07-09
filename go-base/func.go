f7bd538022b979f7fb3fc263855562d9b9822028
huatree <wang.yaohua@foxmail.com>
2022-07-09 20:24:42 +0800

    ✨ feat(go-base): 函数
    1、返回值类型写在最后面；
    2、可返回多个值；
    3、函数作为参数；
    4、没有默认参数、可选参数

--- go-base/func.go
+++ go-base/func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+  "reflect"
+  "runtime"
+)
 
 func eval(a, b int, op string) (int, error) {
   switch op {
@@ -18,8 +23,32 @@ func eval(a, b int, op string) (int, error) {
   }
 }
 
+// 返回多个值
 func div(a, b int) (q, r int) {
   return a / b, a % b
+}
+
+// 函数作为参数
+func apply(op func(int, int) int, a, b int) int {
+  p := reflect.ValueOf(op).Pointer()
+  opName := runtime.FuncForPC(p).Name()
+  fmt.Printf("Calling function %s with args"+
+    "(%d, %d)\n", opName, a, b)
+  return op(a, b)
+}
+
+func pow(a, b int) int {
+  return int(math.Pow(float64(a), float64(b)))
+}
+
+// 可变参数和
+// 没有默认参数，可选参数；只有可变参数列表
+func sum(values ...int) int {
+  s := 0
+  for i := range values {
+    s += values[i]
+  }
+  return s
 }
 
 func main() {
@@ -31,4 +60,12 @@ func main() {
 
   q, r := div(13, 3)
   fmt.Println(q, r)
+
+  fmt.Println(apply(pow, 3, 4))
+  fmt.Println(apply(
+    func(a, b int) int {
+      return int(math.Pow(float64(a), float64(b)))
+    }, 3, 4))
+
+  fmt.Println(sum(1, 3, 4, 6))
 }
